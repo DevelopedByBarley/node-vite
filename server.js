6512ff34431d96bf7c0a4636db666e7d8af20820
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2024-09-12 13:10:31 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// A "dist" mappa kiszolgálása
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Minden más útvonal esetén az index.html kiszolgálása
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
