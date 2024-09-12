a0a749f5e37384539aaf53c57e0d630489f487af
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2024-09-12 13:46:01 +0200

    test 3

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+var cors = require('cors')
+
+
+app.use(cors())
+
 
 // A "dist" mappa kiszolgálása
 
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+
+app.get('/hello', (req, res) => {
+  res.json({
+    'hello': 'hello'
+  })
+})
 // Minden más útvonal esetén az index.html kiszolgálása
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
